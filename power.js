3332241ae135cc54ed64ce39fa76476e2deb51ba
Iryna Nikolenko <irinanikolenko72@gmail.com>
2025-11-04 09:01:56 +0200

    Добавила задание -function power (x,y) в homework3.ja

---
+++ power.js
@@ -0,0 +1,16 @@
+function power (x,y) 
+{
+let result=1; //початкове значення змінної
+for (let i =0; i<y;i++) //повторення циклу "y"разів
+    {
+    result=result*x;
+}
+return result; //повернення результату
+}
+console.log (power(6,4));//1296
+
+
+ 
+
+
+
