faa8e571b38031ea4c86eb58a17090d3ed10fedd
Iryna Nikolenko <irinanikolenko72@gmail.com>
2025-11-21 09:32:42 +0200

    Добавила файл dz10_2.js

---
+++ dz10_2.js
@@ -0,0 +1,8 @@
+const regex = /^[^aA]{6,}$/;
+
+console.log('Wonderful'.match(regex));
+console.log('Joyful'.match(regex));
+console.log('Happiness'.match(regex));
+console.log('Time'.match(regex));
+console.log('Task'.match(regex));
+console.log('Apple'.match(regex));
